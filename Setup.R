6e7f6b6eda63d05cf8bf62dba88f36757c23ed83
la-mar <brocklfriedrich@gmail.com>
2018-08-10 09:51:51 -0500

    formulas

---
+++ Setup.R
@@ -0,0 +1,46 @@
+require(tidyverse)
+require(magrittr)
+require(kableExtra)
+require(car)
+require(knitr)
+require(summarytools)
+require(grid)
+require(gridExtra)
+library(AER)
+require(effects)
+
+# set knitr output options for consistent figures
+knitr::opts_chunk$set(fig.width=12, 
+                      fig.height=8, 
+                      fig.path='../Figs/',
+                      warning=FALSE, 
+                      message=FALSE,
+                      echo = TRUE,
+                      #root.dir = normalizePath(".."),
+                      #child.dir = normalizePath(".."),
+                      cache = FALSE,
+                      results = 'markup',
+                      collapse = TRUE,
+                      tidy.opts = list(width.cutoff=60) # text wrapping
+                      #tidy = TRUE
+                      )
+
+
+
+# set visual theme for the project
+ggplot2::theme_set(ggplot2::theme_bw())
+ggplot2::theme_update(plot.title = ggplot2::element_text(hjust = 0.5))
+
+#prevent implicit conversion to scientific notation
+options(scipen = 999)
+options(knitr.table.format = "html")
+#disable column wrapping
+options(width=100) 
+
+# set options for descriptive statistics
+st_options('escape.pipe', TRUE)
+st_options('descr.stats', c("mean", "sd", "min", "med", "max", "Q1", "Q3", "N.Valid"))
+st_options('descr.transpose', TRUE)
+st_options('style', "rmarkdown")
+
+
--- init.R
+++ init.R
@@ -1,9 +1,10 @@
 require(rmarkdown)
 setwd('./Statistics-Team-Fat-Tails')
 getwd()
-
+options(browser = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe")
 rmarkdown::render('Analysis.Rmd')
-browseURL('Analysis.pdf')
+browseURL('Analysis.pdf', browser = getOption("browser"))
 # browseURL('Analysis.html')
+
 
 
